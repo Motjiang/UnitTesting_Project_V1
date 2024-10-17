c7cb8a41437713adce7a530e98bae062bd5639b6
Motjiang <motjiangbrilliant@gmail.com>
2024-10-17 22:24:08 +0200

    Instantiated the TestClass

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using UnitTesting_Project_V1.Tests;
+
 Console.WriteLine("Hello, World!");
+WorldDumbestFunctionTests.WorldDumbestFunction_ReturnsPickachuIfZero_ReturnsString();
