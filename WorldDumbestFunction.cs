5e04b4f67b3a90fd22b84ebbe9126d28d0890905
Motjiang <motjiangbrilliant@gmail.com>
2024-10-17 22:03:23 +0200

    Created a function that returns a string

---
+++ WorldDumbestFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting_Project_V1
+{
+    public class WorldDumbestFunction
+    {
+        public string ReturnsPikachu(int num)
+        {
+            if(num == 0)
+            {
+                return "Pikachu";
+            }
+            else
+            {
+                return "Squirtle";
+            }
+        }
+    }
+}
